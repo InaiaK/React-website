ba5049e37849a27f75574f66d69ce37b392c1c3d
InaiaKenia <inaiakenia15@gmail.com>
2022-10-12 16:11:45 -0600

    addeD navbar.css button.css button.js - no error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css';
+import {Button} from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960){
+      setButton(false);
+       } else{
+        setButton(true)
+       }
+  };
+
+  window.addEventListener('resize',showButton)
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          INAIA <i className="fab fa-typo3" />
+          IM <i className="fab fa-typo3" />
         </Link>
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -38,11 +51,13 @@ function Navbar() {
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="//signup" className="nav-links" onClick={closeMobileMenu}>
+            <Link to="/signup" className="nav-links-mobile" onClick={closeMobileMenu}>
               Sign-up 
             </Link>
           </li>
         </ul>
+        {/* STATE */}
+        {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
       </div>
     </nav>
   );
