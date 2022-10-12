c2b2f18f65e0c13d1d288a82a79da90e4bd55d67
InaiaKenia <inaiakenia15@gmail.com>
2022-10-12 15:09:49 -0600

    added Link to navbar and app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 export default function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        {/* <Routes>
+        <Navbar/>
+        <Routes>
           <Route path="/" exact >
               INSERT MY REACT COMPONENT HERE
-          </Route>/>
-        </Routes> */}
+          </Route>
+        </Routes>
       </Router>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
+ import { Link } from "react-router-dom";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -9,18 +9,18 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* <Link to="/" className="navbar-logo"> */}
-          {/* INAIA <i className="fab fa-typo3" /> */}
-        {/* </Link> */}
+        <Link to="/" className="navbar-logo">
+          INAIA <i className="fab fa-typo3" />
+        </Link>
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
         Hello!
         <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
-            {/* <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Home
-            </Link> */}
+            </Link>
           </li>
         </ul>
       </div>
