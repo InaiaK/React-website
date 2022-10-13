1057844f47d9f0e5dd4b8a565a1940284c099f4b
InaiaKenia <inaiakenia15@gmail.com>
2022-10-13 16:09:53 -0600

    deleted files and old code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route,Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// navbar componentes links
+import Home from "./components/Navbar/Routes/Home";
+import AboutMe from "./components/Navbar/AboutMe";
+import Portfolio from "./components/Navbar/Portfolio";
+import ContactMe from "./components/Navbar/ContactMe";
 import "./App.css";
 
 export default function App() {
   return (
-    <>
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" exact >
-              INSERT MY REACT COMPONENT HERE
-          </Route>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/aboutme" element={<AboutMe/>}/>
+          <Route path="/portfolio" element={<Portfolio/>}/>
+          <Route path="/contactme" element={<ContactMe/>}/>
         </Routes>
       </Router>
-    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css';
-import {Button} from './Button';
+import "./Navbar.css";
 
-function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960){
-      setButton(false);
-       } else{
-        setButton(true)
-       }
-  };
-
-  window.addEventListener('resize',showButton)
+export default function Navbar() {
 
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          IM <i className="fab fa-typo3" />
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        Hello!
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/home" className="nav-links" onClick={closeMobileMenu}>
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/aboutme" className="nav-links" onClick={closeMobileMenu}>
-            AboutMe
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/projects" className="nav-links" onClick={closeMobileMenu}>
-              Projects
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contactme" className="nav-links" onClick={closeMobileMenu}>
-              ContactMe 
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/signup" className="nav-links-mobile" onClick={closeMobileMenu}>
-              Sign-up 
-            </Link>
-          </li>
-        </ul>
-        {/* STATE */}
-        {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
-      </div>
+      <Link to="/" className="navtitle">
+        Inaia Martin
+      </Link>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/aboutme">AboutMe</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contactme">Contactme</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
-
-export default Navbar;
