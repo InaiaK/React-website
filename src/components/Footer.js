bdae705cd7074709f562f32152c20f97b3dcc19c
InaiaKenia <inaiakenia15@gmail.com>
2022-10-17 16:09:11 -0600

    Added react-bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,40 @@
 import React from "react";
-import {
-  FaMailBulk,
-  FaLinkedin,
-  FaGithub,
-} from "react-icons/fa";
+import { FaMailBulk, FaLinkedin, FaGithub } from "react-icons/fa";
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <div className="main-footer">
       <div className="container">
-        <div className="row">
-          {/* Col1 */}
-          <div className="col">
-            INAIA MARTIN
-            <FaGithub></FaGithub>
-            <a
-              href="https://github.com/InaiaK"
-              className="github"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              GitHub{" "}
-            </a>
-            <FaLinkedin></FaLinkedin>
-            <a
-              href="https://www.linkedin.com/in/inaia-martin/"
-              className="linkedin"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              Linkedin{" "}
-            </a>
-            <FaMailBulk></FaMailBulk> inaiakenia15@gmail.com
-          </div>
-           <hr></hr>
-          <div className="footer-allright">
-            <p>
-              &copy;{new Date().getFullYear()} INAIA MARTIN | All right reserved
-              | Privacy
-            </p>
-          </div>
-        </div>
+        INAIA MARTIN
+        <FaGithub></FaGithub>
+        <a
+          href="https://github.com/InaiaK"
+          className="github"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          GitHub{" "}
+        </a>
+        <FaLinkedin></FaLinkedin>
+        <a
+          href="https://www.linkedin.com/in/inaia-martin/"
+          className="linkedin"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Linkedin{" "}
+        </a>
+        <FaMailBulk></FaMailBulk> inaiakenia15@gmail.com
+      </div>
+      <hr></hr>
+      <div className="footer-container2">
+        <p>
+          &copy;{new Date().getFullYear()} INAIA MARTIN | All right reserved |
+          Privacy
+        </p>
       </div>
     </div>
   );
