cbb1693338249d7ac5d19a15b7eccd5a03f7223a
InaiaKenia <inaiakenia15@gmail.com>
2022-10-17 16:05:11 -0600

    Added react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  FaMailBulk,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 import "./Footer.css";
 
 export default function Footer() {
@@ -8,18 +13,32 @@ export default function Footer() {
         <div className="row">
           {/* Col1 */}
           <div className="col">
-            <h2>INAIA MARTIN</h2>
-            <ul className="list">
-              <li>     
-        <a href="https://github.com/InaiaK" className="github" target="_blank" rel="noreferrer"> GitHub </a></li>
-        <li><a href="https://www.linkedin.com/in/inaia-martin/" className="linkedin" target="_blank" rel="noreferrer"> Linkedin </a></li>
-              <li>inaiakenia15@gmail.com</li>
-            </ul>
+            INAIA MARTIN
+            <FaGithub></FaGithub>
+            <a
+              href="https://github.com/InaiaK"
+              className="github"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              GitHub{" "}
+            </a>
+            <FaLinkedin></FaLinkedin>
+            <a
+              href="https://www.linkedin.com/in/inaia-martin/"
+              className="linkedin"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Linkedin{" "}
+            </a>
+            <FaMailBulk></FaMailBulk> inaiakenia15@gmail.com
           </div>
-          {/* Col2 */}
-          <hr></hr>
-          <div className="row">
-            <p className="col-sm">
+           <hr></hr>
+          <div className="footer-allright">
+            <p>
               &copy;{new Date().getFullYear()} INAIA MARTIN | All right reserved
               | Privacy
             </p>
