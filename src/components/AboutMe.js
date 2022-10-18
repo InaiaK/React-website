f5b5129bfa829aaf2175999c2f61af83a787e578
InaiaKenia <inaiakenia15@gmail.com>
2022-10-18 14:54:03 -0600

    added image to AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import myPicture from "../images/inaia.jpeg"
 
 export default function AboutMe() {
   return (
     <div>
-        <h1> AboutMe</h1>
-        <h2> I am a dedicated professional looking for a front end developer role following my completion of the University of Denver’s FullStack BootCamp. I’m a dynamic learner (I speak native Portuguese, English and Spanish), and hard working (I completed DU’s bootcamp while working full time), and I’m excited to learn and grow in this new career.</h2>
-
-    </div>
-  )
-}
+      <h1> AboutMe</h1>
+      <h2>
+        I am a dedicated professional looking for a front end developer role
+        following my completion of the University of Denver’s FullStack
+        BootCamp. I’m a dynamic learner (I speak native Portuguese, English and
+        Spanish), and hard working (I completed DU’s bootcamp while working full
+        time), and I’m excited to learn and grow in this new career. Currently
+        working as a Freelance to improve my skills.
+      </h2>
+      </div>
+    // {/* </div>
+    // <div className="inaia-picture">
+    // <img src={myPicture} className="img" alt="Inaia"/>
+    // </div> */}
+  );
+};
