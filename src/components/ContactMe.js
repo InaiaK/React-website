edc3962f7a45fb0aef29d84ba779a0753a23300b
InaiaKenia <inaiakenia15@gmail.com>
2022-10-18 14:45:08 -0600

    added contact comments

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,15 +1,34 @@
-import React from 'react'
-
+import React from "react";
+// CHECK TO ADD > import { Form } from "react-bootstrap";
 
 export default function ContactMe() {
   return (
     <div>
-        <h1>ContactMe </h1>
+      <h1>ContactMe </h1>
+      <p>Social Media</p>
+      <a
+        href="https://github.com/InaiaK"
+        className="github"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        GitHub{" "}
+      </a>
+      <a
+        href="https://www.linkedin.com/in/inaia-martin/"
+        className="linkedin"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Linkedin{" "}
+      </a>
 
-        <p>Social Media</p>
-        <a href="https://github.com/InaiaK" className="github" target="_blank" rel="noreferrer"> GitHub </a>
-        <a href="https://www.linkedin.com/in/inaia-martin/" className="linkedin" target="_blank" rel="noreferrer"> Linkedin </a>
-
+      {/* CREATE DIV FORM 
+ADD BACKGROUND IMG OR MP4
+  CREATE FUNCTION FOR EMAIL.JS 
+  CREATE FORM SPACE  */}
     </div>
-  )
+  );
 }
