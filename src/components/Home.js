15747b177f302dcf4b8ce98427620db8a6de8266
InaiaKenia <inaiakenia15@gmail.com>
2022-10-21 19:15:53 -0600

    moved imgages and videos > new folder assets

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hey there, I'm Inaia Martin.</h1>
-      <h2> Software Engernier</h2>
+      <h2> Software Engineer</h2>
     </div>
   );
 }
