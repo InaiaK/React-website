9f7dbf5c13fb979a5112694fdccf828cbdc34c1e
InaiaKenia <inaiakenia15@gmail.com>
2022-10-26 11:52:27 -0600

    added video home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import "./Home.css";
-// import Video from "../src/assets/video/background2.mp4";
+import Video from "../assets/video/background2.mp4";
 
 
 //functional component.
-
  function Home() {
   return (
-    <>
-          {/* <video className="video" src={Video} autoPlay loop /> */}
-        <div className="header">
-      <h1>Hey there,<span>I'm Inaia Martin.</span> </h1>
+    <>  <div className="header">
+        <video className="video" src={Video} autoPlay loop muted
+        style={{position:"absolute", objectFit:"cover", zIndex:"-1"}}/> 
+        <h1>Inaia Martin</h1>
       <h2> Software Engineer</h2>
     </div>
     </>
