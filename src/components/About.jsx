40a7813ce6d59e9f2ad78828090636a3393c9817
InaiaKenia <inaiakenia15@gmail.com>
2022-10-26 20:40:10 -0600

    updated resume external link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,18 @@ function About() {
           muted
           style={{ position: "absolute", objectFit: "cover", zIndex: "-1" }}
         />
-
+ <Card.Body>
+            <Card.Title>Inaia Martin</Card.Title>
+            <Card.Text>
+              I am a dedicated professional looking for a front end developer
+              role following my completion of the University of Denver’s
+              FullStack BootCamp. I’m a dynamic learner (I speak native
+              Portuguese, English and Spanish), and hard working (I completed
+              DU’s bootcamp while working full time), and I’m excited to learn
+              and grow in this new career. Currently working as a Freelance to
+              improve my skills.
+            </Card.Text>
+          </Card.Body>
         <Card className="Cardaboutme">
           <Card.Img
             style={{ width: "15rem", border: "15" }}
@@ -61,18 +72,7 @@ function About() {
         Linkedin{" "}
       </a>
       <FaMailBulk></FaMailBulk> inaiakenia15@gmail.com
-          <Card.Body>
-            <Card.Title>Inaia Martin</Card.Title>
-            <Card.Text>
-              I am a dedicated professional looking for a front end developer
-              role following my completion of the University of Denver’s
-              FullStack BootCamp. I’m a dynamic learner (I speak native
-              Portuguese, English and Spanish), and hard working (I completed
-              DU’s bootcamp while working full time), and I’m excited to learn
-              and grow in this new career. Currently working as a Freelance to
-              improve my skills.
-            </Card.Text>
-          </Card.Body>
+         
         </Card>
       </div>
     </>
