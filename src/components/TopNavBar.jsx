5ecceb95549d1ea5760418c9763e716da3497d4a
InaiaKenia <inaiakenia15@gmail.com>
2022-10-26 20:22:20 -0600

    organized imports from TopNavBar.jsx

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "react-bootstrap";
 import "./TopNavBar.css";
+// Componentes
 import "./Home";
 import "./About";
 import "./Portfolio";
