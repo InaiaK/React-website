b4facbf3ea3891efe10b517255ec79965c28f6f7
InaiaKenia <inaiakenia15@gmail.com>
2022-10-28 17:51:00 -0600

    fixed Coming data

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Card, Button} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Portfolio.css";
 //Assets
 import Real from "../assets/images/realfakeeb.gif";
@@ -8,70 +8,60 @@ import Note from "../assets/images/note.gif";
 import Password from "../assets/images/password.gif";
 import Coming from "../assets/images/coming.gif";
 
-
 const Portfolio = () => {
   const cardData = [
-  {
-    img: Real,
-    altText:"app.gif",
-    title:"Real Fake Store",
-    deployed:"https://real-fake-store.herokuapp.com/",
-    description:"A place for intergalactic travelers to find the products they need to keep sane on their long journeys through space.Whether they want something unique or something real fake, the real fake store has it all. Our application is web responsive, easily visible on a mobile device or from your spaceship’s console.",
-  },
-  {
-    img: Note,
-    altText:"app.gif",
-    title:"Note Taker",
-    deployed:"https://tranquil-dawn-45157.herokuapp.com/",
-    description:"Handy Note Taker live deployed on Heroku which will allow the user to write and store notes!",
+    {
+      img: Real,
+      altText: "app.gif",
+      title: "Real Fake Store",
+      deployed: "https://real-fake-store.herokuapp.com/",
+      description:
+        "A place for intergalactic travelers to find the products they need to keep sane on their long journeys through space.Whether they want something unique or something real fake, the real fake store has it all. Our application is web responsive, easily visible on a mobile device or from your spaceship’s console.",
+    },
+    {
+      img: Note,
+      altText: "app.gif",
+      title: "Note Taker",
+      deployed: "https://tranquil-dawn-45157.herokuapp.com/",
+      description:
+        "Handy Note Taker live deployed on Heroku which will allow the user to write and store notes!",
+    },
 
-  },
-  
-  {
-    img: Password,
-    altText:"app.gif",
-    title:"Password Generator",
-    deployed:"https://inaiak.github.io/Password-generator/",
-    description:" An application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code.clean and polished, responsive user interface that adapts to multiple screen sizes.",
+    {
+      img: Password,
+      altText: "app.gif",
+      title: "Password Generator",
+      deployed: "https://inaiak.github.io/Password-generator/",
+      description:
+        " An application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code.clean and polished, responsive user interface that adapts to multiple screen sizes.",
+    },
+    {
+      img: Coming,
+      altText: "app.gif",
+      title: "Future Project",
+      deployed: " ",
+      description: " ",
+    },
+  ];
 
-  },
-  {
-    img: Coming,
-    altText:"app.gif",
-    title:"Future Project",
-    deployed:"-",
-    description:" ",
-
-  }
-
-];
-
-
-const renderCard = (card,index)  => {
-  return (
-  <Card style={{ width: '20rem' }} key={index} className="card.box">
-      <Card.Img variant="top" src={card.img} alt={card.altText}/>
-      <Card.Body>
-        <Card.Title>{card.title}</Card.Title>
-        <Card.Text>
-    {card.description}
-        </Card.Text>
-        <Button >
-          <a href={card.deployed}
-          target="_blank"
-          rel="noreferrer noopener">
-            Visit {card.title}
-          </a>
+  const renderCard = (card, index) => {
+    return (
+      <Card style={{ width: "20rem" }} key={index} className="card.box">
+        <Card.Img variant="top" src={card.img} alt={card.altText} />
+        <Card.Body>
+          <Card.Title>{card.title}</Card.Title>
+          <Card.Text>{card.description}</Card.Text>
+          <Button>
+            <a href={card.deployed} target="_blank" rel="noreferrer noopener">
+              Visit {card.title}
+            </a>
           </Button>
-      </Card.Body>
-    </Card>
-  );
+        </Card.Body>
+      </Card>
+    );
+  };
+
+  return <div className="grid"> {cardData.map(renderCard)}</div>;
 };
 
-return (
-<div className='grid'> {cardData.map(renderCard)}</div>
-);
-};
-   
-            
 export default Portfolio;
