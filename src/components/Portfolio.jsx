389dc5bfeb94b7563689ef45a13561c04e94614c
InaiaKenia <inaiakenia15@gmail.com>
2022-10-28 17:32:11 -0600

    added cardData & renderCard

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { Card, CardGroup} from "react-bootstrap";
+import { Card, Button} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Portfolio.css";
 //Assets
@@ -9,9 +9,9 @@ import Note from "../assets/images/note.gif";
 // import Coming from "../assets/images/coming.gif";
 
 
-const portfolioData =[
+const Portfolio = () => {
+  const cardData = [
   {
-    key:"card1",
     img: Real,
     altText:"picture",
     title:"Real Fake Store",
@@ -19,60 +19,35 @@ const portfolioData =[
     description:"A place for intergalactic travelers to find the products they need to keep sane on their long journeys through space.Whether they want something unique or something real fake, the real fake store has it all. Our application is web responsive, easily visible on a mobile device or from your spaceship’s console.",
   },
   {
-    key:"card2",
     img: Note,
     altText:"picture",
-    title:"Real Fake Store",
+    title:"Note Taker",
     deployed:"https://real-fake-store.herokuapp.com/",
     description:"A place for intergalactic travelers to find the products they need to keep sane on their long journeys through space.Whether they want something unique or something real fake, the real fake store has it all. Our application is web responsive, easily visible on a mobile device or from your spaceship’s console.",
 
   }
 ];
 
-      const Portfolio = () => {
-        return (
-          <main>
-            <section className="gridRow">
-              {portfolioData.map(
-                ({
-                  key,
-                  img,
-                  altText,
-                  title,
-                  deployed,
-                  description,
-                }) => {
-                  return (
-                    <div key={key} className="card">
-                      <img className="card-img-top" src={img} alt={altText} />
-                      <div className="card-body">
-                        <h5 className="text-center">
-                          <a
-                            className="card-title text-white"
-                            href={deployed}
-                            target="_blank"
-                            rel="noreferrer noopener"
-                          >
-                           
-                          </a>
-                        </h5>{title}
-                        <p className="card-text text-center">   {description}</p>
-                        <a
-                          href={deployed}
-                          className="btn btn-primary"
-                          target="_blank"
-                          rel="noreferrer noopener"
-                        >
-                          Github Page
-                        </a>
-                      </div>
-                    </div>
-                  );
-                }
-              )}
-            </section>
-          </main>
-        );
-      };
-      
-      export default Portfolio;
+
+const renderCard = (card,index)  => {
+  return (
+  <Card style={{ width: '18rem' }} key={index}>
+      <Card.Img variant="top" src={card.img} alt={card.altText}/>
+      <Card.Body>
+        <Card.Title>{card.title}</Card.Title>
+        <Card.Text>
+    {card.description}
+        </Card.Text>
+        <Button variant="primary">{card.deployed}</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+return (
+<div className='grid'> {cardData.map(renderCard)}</div>
+);
+};
+   
+            
+export default Portfolio;
