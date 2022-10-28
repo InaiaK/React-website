f2b535b31049720a363a1fe9a8dbdceff6572809
InaiaKenia <inaiakenia15@gmail.com>
2022-10-28 16:23:13 -0600

    created portfolioData

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,97 +9,110 @@ import Password from "../assets/images/password.gif";
 import Coming from "../assets/images/coming.gif";
 
 
+const portfolioData =[
+  {
+    id:"card1",
+    img: Real,
+    title:"Real Fake Store",
+    deployed:"https://real-fake-store.herokuapp.com/",
+    description:"A place for intergalactic travelers to find the products they need to keep sane on their long journeys through space.Whether they want something unique or something real fake, the real fake store has it all. Our application is web responsive, easily visible on a mobile device or from your spaceship’s console.",
+  },
+];
 
-function Portfolio() {
-     return (
- 
-      <CardGroup className='card-portfolio'>
-      {/* CARD 01 */}
+const Portfolio = () => {
+     return ( 
+
+<CardGroup>
+  {portfolioData.map (
+    ({
+      id,
+      img,
+      title,
+      deployed,
+      description,
+    }) => {
+      return (
+
         <Card>
-          <Card.Img  className='img-portfolio' variant="top" src={Real}/>
-          <Card.Body className='card-portfolio'>
-            <Card.Title><a
-          href="https://real-fake-store.herokuapp.com/"
-          className="event-link"
+<Card.Img  className='img-portfolio' variant="top" src={img}/>
+ <Card.Body className='card-portfolio'>
+          <Card.Title><a
+          href={deployed}
+          className="real-link"
           target="_blank"
           rel="noreferrer"
         >
-          {" "}RealFake Store{" "}
+          {title}
         </a> </Card.Title>
             <Card.Text>
-            A place for intergalactic travelers to find the products they need to keep sane on their long journeys through space.
-            Whether they want something unique or something real fake, the real fake store has it all. Our application is web responsive, easily visible on a mobile device or from your spaceship’s console.
+            {description}
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
 
-          <small className="text-muted">Technologies: MongoDB, Express.js,Node.js, React.js, Heroku & GraphQL 
-              
-            </small>
-          </Card.Footer>
-        </Card>
+    }
 
-{/* CARD 02  */}
 
-        <Card>
-          <Card.Img className='img-portfolio' variant="top" src={Password} />
-          <Card.Body className='card-portfolio'>
-            <Card.Title><a
-          href="https://inaiak.github.io/Password-generator/"
-          className="event-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}Password Generator{" "}
-        </a></Card.Title>
-            <Card.Text>
-            An application that enables employees to generate random passwords based on criteria that they’ve selected. 
-            This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.{' '}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Technologies:HTML, CSS, JavaScript</small>
-          </Card.Footer>
-        </Card>
 
-        {/* CARD 03 */}
-        <Card>
-          <Card.Img className='img-portfolio' variant="top" src={Note} />
-          <Card.Body className='card-portfolio'>
-            <Card.Title><a
-          href="https://tranquil-dawn-45157.herokuapp.com/"
-          className="event-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}Note Taker {" "}
-        </a></Card.Title>
-            <Card.Text>
-            Note Taker that can be used to write and save notes.
-                       </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Technologies:Bootstrap, JavaScript, Express.js</small>
-          </Card.Footer>
-        </Card>
-        {/* CARD 04  */}
 
-        <Card>
-          <Card.Img className='img-portfolio' variant="top" src={Coming} />
-          <Card.Body>
-            <Card.Title>Future Project</Card.Title>
-            <Card.Text>
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Technologies</small>
-          </Card.Footer>
-        </Card>       
-      </CardGroup>
-    );
-  }
+  
   
 export default Portfolio;  
+
+
+
+
+//           href="https://inaiak.github.io/Password-generator/"
+//           className="event-link"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           {" "}Password Generator{" "}
+//         </a></Card.Title>
+//             <Card.Text>
+//             An application that enables employees to generate random passwords based on criteria that they’ve selected. 
+//             This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.{' '}
+//             </Card.Text>
+//           </Card.Body>
+//           <Card.Footer>
+//             <small className="text-muted">Technologies:HTML, CSS, JavaScript</small>
+//           </Card.Footer>
+//         </Card>
+
+//         {/* CARD 03 */}
+//         <Card>
+//           <Card.Img className='img-portfolio' variant="top" src={Note} />
+//           <Card.Body className='card-portfolio'>
+//             <Card.Title><a
+//           href="https://tranquil-dawn-45157.herokuapp.com/"
+//           className="event-link"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           {" "}Note Taker {" "}
+//         </a></Card.Title>
+//             <Card.Text>
+//             Note Taker that can be used to write and save notes.
+//                        </Card.Text>
+//           </Card.Body>
+//           <Card.Footer>
+//             <small className="text-muted">Technologies:Bootstrap, JavaScript, Express.js</small>
+//           </Card.Footer>
+//         </Card>
+//         {/* CARD 04  */}
+
+//         <Card>
+//           <Card.Img className='img-portfolio' variant="top" src={Coming} />
+//           <Card.Body>
+//             <Card.Title>Future Project</Card.Title>
+//             <Card.Text>
+//             </Card.Text>
+//           </Card.Body>
+//           <Card.Footer>
+//             <small className="text-muted">Technologies</small>
+//           </Card.Footer>
+//         </Card>       
+//       </CardGroup>
+   
 
 
 
