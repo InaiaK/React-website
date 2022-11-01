247c4ec2bf3b966b03e73bd4ff1b36638a591bbe
InaiaKenia <inaiakenia15@gmail.com>
2022-11-01 15:44:08 -0600

    fixed constants names from portfolio and altText

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,24 +3,24 @@ import { Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Portfolio.css";
 // Assets
-import Real from "../assets/images/realfakeeb.gif";
-import Note from "../assets/images/note.gif";
-import Password from "../assets/images/password.gif";
-import Coming from "../assets/images/coming.gif";
+import REAL from "../assets/images/realfakeeb.gif";
+import NOTE from "../assets/images/note.gif";
+import PASSWORD from "../assets/images/password.gif";
+import COMING from "../assets/images/coming.gif";
 
 const Portfolio = () => {
   const cardData = [
     {
-      img: Real,
-      altText: "app.gif",
+      img: REAL,
+      altText: "An animation showing use of the Real Fake Store app",
       title: "Real Fake Store",
       deployed: "https://real-fake-store.herokuapp.com/",
       description:
         "A place for intergalactic travelers to find the products they need to keep sane on their long journeys through space.Whether they want something unique or something real fake, the real fake store has it all. Our application is web responsive, easily visible on a mobile device or from your spaceship’s console.",
     },
     {
-      img: Note,
-      altText: "app.gif",
+      img: NOTE,
+      altText: "An animation showing use of the Note app",
       title: "Note Taker",
       deployed: "https://tranquil-dawn-45157.herokuapp.com/",
       description:
@@ -28,16 +28,16 @@ const Portfolio = () => {
     },
 
     {
-      img: Password,
-      altText: "app.gif",
+      img: PASSWORD,
+      altText: "An animation showing use of the Password app",
       title: "Password Generator",
       deployed: "https://inaiak.github.io/Password-generator/",
       description:
         " An application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code.clean and polished, responsive user interface that adapts to multiple screen sizes.",
     },
     {
-      img: Coming,
-      altText: "app.gif",
+      img: COMING,
+      altText: "An animation showing coming word",
       title: "Future Project",
       deployed: " ",
       description: " ",
