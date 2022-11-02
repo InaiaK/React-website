77f68a6c9ede77027534eb898d025d4517133148
InaiaKenia <inaiakenia15@gmail.com>
2022-11-02 15:10:17 -0600

    deleted carousel & added Card from react bootstrap

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,58 +1,50 @@
 import React from "react";
-import {Carousel} from "react-bootstrap";
+import { Card } from "react-bootstrap";
 //import { FaMailBulk, FaLinkedin, FaGithub, FaSave } from "react-icons/fa";
 import "./About.css";
 // Assets
-// import INAIA_IMG from "../assets/images/inaia.jpeg";
+import INAIA_IMG from "../assets/images/inaia.jpeg";
+import MERN from "../assets/images/MERN1.png";
 // import QR_CODE from "../assets/images/qrcode.png";
-// import VIDEO from "../assets/video/background2.mp4";
+//import VIDEO from "../assets/video/background2.mp4";
 
 // Functional component.
 
+const About = () => {
+  const aboutData = [
+    {
+      img: MERN,
+      altText: "image of MERN stack as my skills",
+      title: "Skills",
+      description: "skills",
+    },
+    {
+      img: INAIA_IMG,
+      altText: "image of MERN stack as my skills",
+      title: "Skills",
+      description: "skills",
+    },
+    {
+      img: MERN,
+      altText: "image of MERN stack as my skills",
+      title: "Skills",
+      description: "skills",
+    },
+  ];
 
+  const renderAbout = ({ img, altText, title, description }, index) => {
+    return (
+      <Card key={`about-card-${index}`} className="about-box">
+        <Card.Img src={img} alt={altText} />
+        <Card.Body className="card-body">
+          {title}
+          <Card.Text>{description}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  };
 
-function About () {
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
+  return <div className="grid"> {aboutData.map(renderAbout)}</div>;
+};
 
 export default About;
