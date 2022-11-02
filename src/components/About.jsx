4c5f688d36e5fef2ac12f473009addc5348bb220
InaiaKenia <inaiakenia15@gmail.com>
2022-11-02 14:07:01 -0600

    added Carousel - react bootstrap

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,81 +1,57 @@
 import React from "react";
-import { FaMailBulk, FaLinkedin, FaGithub, FaSave } from "react-icons/fa";
+import {Carousel} from "react-bootstrap";
+//import { FaMailBulk, FaLinkedin, FaGithub, FaSave } from "react-icons/fa";
 import "./About.css";
 // Assets
-import INAIA_IMG from "../assets/images/inaia.jpeg";
-import QR_CODE from "../assets/images/qrcode.png";
-import VIDEO from "../assets/video/background2.mp4";
+// import INAIA_IMG from "../assets/images/inaia.jpeg";
+// import QR_CODE from "../assets/images/qrcode.png";
+// import VIDEO from "../assets/video/background2.mp4";
 
 // Functional component.
-function About() {
+
+
+
+function About () {
   return (
-    <>
-      <div>
-        <video className="video" src={VIDEO} autoPlay loop muted />
-        <section className="about-body">
-          <h1>Inaia Martin</h1>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
           <p>
-            I am a dedicated professional looking for a front end developer role
-            following my completion of the University of Denver’s FullStack
-            BootCamp. I’m a dynamic learner (I speak native Portuguese, English
-            and Spanish), and hard working (I completed DU’s bootcamp while
-            working full time), and I’m excited to learn and grow in this new
-            career. Currently working as a Freelance to improve my skills.
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
-          <img className="img-about-me" src={INAIA_IMG} alt="Profile" />
-          <img
-            className="img-qrcode"
-            src={QR_CODE}
-            alt="QR_CODE for my resume"
-          />
-        </section>
-        <div className="contact"></div>
-        <ul>
-          {" "}
-          Contact
-          <li>
-            {" "}
-            <FaSave></FaSave>
-            <a
-              href="https://docs.google.com/document/d/1yOadZ6xjOhjUliVfG67TEzYHTqOhzdbc/edit#heading=h.gjdgxs"
-              className="resume"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Resume
-            </a>
-          </li>
-          <li>
-            {" "}
-            <FaGithub></FaGithub>
-            <a
-              href="https://github.com/InaiaK"
-              className="github"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              GitHub{" "}
-            </a>
-          </li>
-          <li>
-            <FaLinkedin></FaLinkedin>
-            <a
-              href="https://www.linkedin.com/in/inaia-martin/"
-              className="linkedin"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              Linkedin{" "}
-            </a>
-          </li>
-          <li>
-            <FaMailBulk></FaMailBulk> inaiakenia15@gmail.com
-          </li>
-        </ul>
-      </div>
-    </>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
