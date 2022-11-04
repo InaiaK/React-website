75b94e25134403c9833eeedd6be0f5ae5e2e78f0
InaiaKenia <inaiakenia15@gmail.com>
2022-11-04 10:15:57 -0600

    added two img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,33 +16,34 @@ const About = () => {
       img: MERN,
       altText: "image of MERN stack as my skills",
       title: "Skills",
-      description: "skills",
+      description:"skills",
     },
     {
       img: INAIA_IMG,
       altText: "image of MERN stack as my skills",
       title: "Skills",
-      description: "skills",
+      description:"skills",
     },
     {
       img: MERN,
       altText: "image of MERN stack as my skills",
       title: "Skills",
-      description: "skills",
+      description:"skills",
     },
   ];
 
   const renderAbout = ({ img, altText, title, description }, index) => {
     return (
-      <Card key={`about-card-${index}`} className="about-box">
-        <Card.Img src={img} alt={altText} />
+<Card key={`about-card-${index}`} className="about-box">
+        <Card.Img  src={img} alt={altText} />
         <Card.Body className="card-body">
-          {title}
+{title}
           <Card.Text>{description}</Card.Text>
         </Card.Body>
       </Card>
     );
   };
+
 
   return <div className="grid"> {aboutData.map(renderAbout)}</div>;
 };
